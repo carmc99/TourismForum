d5192202ee5209bdb086476d3f0713841d620ba2
carmc99 <carmc99@gmail.com>
2022-04-11 14:30:08 -0500

    Modificado variables de entorno

--- pages/posts/register.tsx
+++ pages/posts/register.tsx
@@ -98,7 +98,9 @@ const RegisterForm = () => {
             >
               <option value="">Seleccione una opcion</option>
               {BIOMES.map((biome) => (
-                <option key={biome} value={biome}>{biome}</option>
+                <option key={biome} value={biome}>
+                  {biome}
+                </option>
               ))}
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
