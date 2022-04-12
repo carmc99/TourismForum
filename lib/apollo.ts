0b2d2b149f038ae6acf107aaa694395f486355cd
carmc99 <carmc99@gmail.com>
2022-04-11 19:30:23 -0500

    Modificado url base cliente apollo

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,8 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
 
 export const client = new ApolloClient({
-  uri: 'http://tourism-forum.vercel.app/api/graphql',
-  //uri: 'http://localhost:3000/api/graphql',
+  uri: 'https://tourism-forum.vercel.app/api/graphql',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
